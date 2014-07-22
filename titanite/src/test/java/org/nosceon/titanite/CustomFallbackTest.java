ff18d6ed5a39067827784488941efd0fe12ec49e
Johan Siebens <johan.siebens@gmail.com>
2014-07-22 19:27:12 +0200

    proper fallback test

--- titanite/src/test/java/org/nosceon/titanite/CustomFallbackTest.java
+++ titanite/src/test/java/org/nosceon/titanite/CustomFallbackTest.java
@@ -17,10 +17,15 @@ package org.nosceon.titanite;
 
 import org.junit.Test;
 
+import java.util.concurrent.CompletionStage;
+import java.util.function.Function;
+
 import static com.jayway.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.nosceon.titanite.Method.GET;
 import static org.nosceon.titanite.Method.POST;
+import static org.nosceon.titanite.Titanite.$;
+import static org.nosceon.titanite.Titanite.Responses.notFound;
 import static org.nosceon.titanite.Titanite.Responses.ok;
 
 /**
@@ -28,24 +33,40 @@ import static org.nosceon.titanite.Titanite.Responses.ok;
  */
 public class CustomFallbackTest extends AbstractE2ETest {
 
+    public static class Handler implements Function<Request, CompletionStage<Response>> {
+
+        private String value;
+
+        public Handler(String value) {
+            this.value = value;
+        }
+
+        @Override
+        public CompletionStage<Response> apply(Request request) {
+            if (value.equals(request.queryParams().getString("v"))) {
+                return ok().body(value).toFuture();
+            }
+            else {
+                return notFound().toFuture();
+            }
+        }
+
+    }
+
     @Override
     protected Shutdownable configureAndStartHttpServer(HttpServer server) {
         return
             server
-                .register(GET, "/a/b", (r) -> ok().text("A").toFuture())
-                .register(POST, "/a/*path", (r) -> ok().text("B").toFuture())
-                .register(GET, "/a/*path", (r) -> ok().text("C").toFuture())
+                .register(GET, "/a", $(new Handler("A"), new Handler("B"), new Handler("C")))
                 .start();
     }
 
     @Test
     public void test() {
-        given().expect().statusCode(200).body(equalTo("A")).when().get(uri("/a/b"));
-        given().expect().statusCode(200).body(equalTo("B")).when().post(uri("/a/b"));
-        given().expect().statusCode(405).when().put(uri("/a/b"));
-
-        given().expect().statusCode(200).body(equalTo("C")).when().get(uri("/a/c"));
-        given().expect().statusCode(404).when().get(uri("/b/c"));
+        given().queryParam("v", "A").expect().statusCode(200).body(equalTo("A")).when().get(uri("/a"));
+        given().queryParam("v", "B").expect().statusCode(200).body(equalTo("B")).when().get(uri("/a"));
+        given().queryParam("v", "C").expect().statusCode(200).body(equalTo("C")).when().get(uri("/a"));
+        given().queryParam("v", "X").expect().statusCode(404).when().get(uri("/a"));
     }
 
 }
